6a786c8d13b949321b2b3c384c585182d868359d
Victor Mykhailiv <victor@feelsoftware.com>
2022-11-21 13:12:23 +0200

    Disable send logs to Crashlytics in debug mode

--- lib/logger.dart
+++ lib/logger.dart
@@ -1,19 +1,28 @@
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:logger/logger.dart' as logger_impl;
 
+/// Log to Logcat only in debug mode, send logs to Crashlytics only in release mode.
 class Logger {
   static final logger_impl.Logger _loggerImpl = logger_impl.Logger();
 
   Logger._();
 
   static void d(String message) {
-    _loggerImpl.d(message);
+    if (kDebugMode) {
+      _loggerImpl.d(message);
+    }
+
+    if (!kReleaseMode) return;
     FirebaseCrashlytics.instance.log(message);
   }
 
   static void e(String message, [dynamic error, StackTrace? stackTrace]) {
-    _loggerImpl.e(message, error, stackTrace);
+    if (kDebugMode) {
+      _loggerImpl.e(message, error, stackTrace);
+    }
+
+    if (!kReleaseMode) return;
     FirebaseCrashlytics.instance.recordFlutterFatalError(FlutterErrorDetails(
       exception: Exception(message + error.toString()),
       stack: stackTrace,
